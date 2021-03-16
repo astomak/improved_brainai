70dc1f03f059ce1cd45b138af46a7fbef939fbd5
astomak <harshakaastomak@gmail.com>
2021-03-16 20:09:07 +0530

    Updates

---
+++ precache-manifest.70ff9fbf4916b61ccb12f0e902ebfc6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7636f65c1f5d8fddd95cef0426ca37",
+    "url": "/improved_brainai/index.html"
+  },
+  {
+    "revision": "dc9f84b160a866076bbb",
+    "url": "/improved_brainai/static/css/main.0ddc3f83.chunk.css"
+  },
+  {
+    "revision": "791cfd24a54421a59fa6",
+    "url": "/improved_brainai/static/js/2.a1d0551a.chunk.js"
+  },
+  {
+    "revision": "229313a91dfc1a4229b4758368bd8914",
+    "url": "/improved_brainai/static/js/2.a1d0551a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9f84b160a866076bbb",
+    "url": "/improved_brainai/static/js/main.5d318860.chunk.js"
+  },
+  {
+    "revision": "d3f24e4bcf1576ecafa1",
+    "url": "/improved_brainai/static/js/runtime-main.d214c7db.js"
+  }
+]);
